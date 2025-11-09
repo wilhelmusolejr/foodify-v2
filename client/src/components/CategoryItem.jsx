954f6bd88dfd519bba1b320433198edb447e7deb
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-11-10 00:36:28 +0800

    refactor: modify temporary image to actual image

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function CategoryItem() {
-  return <div className="w-30 h-30 bg-red-300 rounded-full"></div>;
+export default function CategoryItem({ image_path, title }) {
+  return (
+    <div className="text-center flex flex-col items-center gap-2">
+      {/* image */}
+      <div className="w-30 h-30 rounded-full">
+        <img
+          src={image_path}
+          alt={`Image for header food item`}
+          className=" object-cover object-center w-full h-full rounded-sm"
+        />
+      </div>
+
+      <p>{title}</p>
+    </div>
+  );
 }
