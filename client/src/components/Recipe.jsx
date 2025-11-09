700ed7978df6de5167cd6cc571921cd53dbb8dac
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-11-10 00:35:21 +0800

    refactor: modify temporary image to actual image

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-export default function Recipe({ image, link, name }) {
+export default function Recipe({ image_path, link, name }) {
   return (
     <>
       <div className="">
-        <div class="bg-red-300 h-32 rounded-lg flex items-center justify-center"></div>
+        <div class="h-32 rounded-lg flex items-center justify-center">
+          <img
+            src={image_path}
+            alt={`Image for header food item`}
+            className=" object-cover object-center w-full h-full rounded-sm"
+          />
+        </div>
         <h2 className="capitalize mt-2 text-lg font-medium">{name}</h2>
       </div>
     </>
