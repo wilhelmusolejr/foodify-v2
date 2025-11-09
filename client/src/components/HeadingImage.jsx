e3647eea1744c843b39bc1f8d5fba0213eabad15
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-11-10 00:35:33 +0800

    refactor: modify temporary image to actual image

--- client/src/components/HeadingImage.jsx
+++ client/src/components/HeadingImage.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function HeadingImage({ color }) {
-  return <div className={`min-w-40 h-40 rounded-md ${color} `}></div>;
+export default function HeadingImage({ image_path = "" }) {
+  return (
+    <div className={`min-w-40 h-40 rounded-md`}>
+      <img
+        src={image_path}
+        alt={`Image for header food item`}
+        className=" object-cover object-center w-full h-full rounded-sm"
+      />
+    </div>
+  );
 }
