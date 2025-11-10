4802edf7544af3563f1e76f17174fc61ea8ad1e1
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-10 07:12:38 -0500

    style: adjust font size for the heading h2

--- client/src/components/SectionHeading.jsx
+++ client/src/components/SectionHeading.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function SectionHeading({ heading, subheading }) {
   return (
-    <div className="text-center capitalize flex gap-2 flex-col mb-10">
+    <div className="text-center capitalize flex gap-2 flex-col mb-14">
       <p className="italic w-8/12 mx-auto">{subheading}</p>
-      <h2 className="text-2xl font-semibold">{heading}</h2>
+      <h2 className="text-2xl md:text-3xl font-semibold">{heading}</h2>
     </div>
   );
 }
