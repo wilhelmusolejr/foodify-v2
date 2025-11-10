1c39f293c426afd22a7c829cf562ae78fd22a309
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-10 06:47:31 -0500

    feat(components): add new logo component.

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <>
+    <a href="/" className="logo">
+          <img src="images/logo.png" alt="" className="w-20" />
+        </a>
+    </>
+  );
+}
