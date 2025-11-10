9d8deb7955a8156a450cd531c9cef1804eeecf30
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-10 07:34:25 -0500

    style: adjust image size

--- client/src/components/BlogItem.jsx
+++ client/src/components/BlogItem.jsx
@@ -3,25 +3,27 @@ import React from "react";
 export default function BlogItem({ heading, date, description, imageUrl }) {
   return (
     <div className="my-20 border-b border-black/20 pb-20">
-      {/* image */}
-      <div className="h-50">
-        <img
-          src={`images/blog/${imageUrl}`}
-          alt={`Image for ${heading}`}
-          className=" object-cover object-center w-full h-full rounded-sm"
-        />
-      </div>
+      <div className="flex gap-5 flex-col md:flex-row">
+        {/* image */}
+        <div className="h-50 md:min-w-50 md:h-50">
+          <img
+            src={`images/blog/${imageUrl}`}
+            alt={`Image for ${heading}`}
+            className=" object-cover object-center w-full h-full rounded-sm"
+          />
+        </div>
 
-      {/* data */}
-      <div className="pt-5">
-        <h3 className="mb-2">{date}</h3>
-        <h2 className="font-bold text-2xl mb-2 poltawski-nowy text-[#2B4A13]">
-          {heading}
-        </h2>
-        <p className="mb-10  text-[#333]">{description}</p>
-        <a href="#" className="underline capitalize text-xl text-[#2B4A13]">
-          Continue Reading
-        </a>
+        {/* data */}
+        <div className="pt-5">
+          <h3 className="mb-2">{date}</h3>
+          <h2 className="font-bold text-2xl mb-2 poltawski-nowy text-[#2B4A13]">
+            {heading}
+          </h2>
+          <p className="mb-10  text-[#333]">{description}</p>
+          <a href="#" className="underline capitalize text-xl text-[#2B4A13]">
+            Continue Reading
+          </a>
+        </div>
       </div>
     </div>
   );
