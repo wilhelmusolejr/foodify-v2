b32800a074bb7c77f2499de5c0a352c41bd79943
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-11 04:45:58 -0500

    style: adjust color and font size

--- client/src/components/StatCard.jsx
+++ client/src/components/StatCard.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function StatCard({ number, label }) {
   return (
     <div className="stat-card">
-      <h2 className="text-2xl text-yellow-500">{number}</h2>
-      <p>{label}</p>
+      <h2 className="text-2xl text-yellow-500 xl:text-3xl font-semibold tracking-wide">{number}</h2>
+      <p className="tracking-wide text-lg">{label}</p>
     </div>
   );
 }
