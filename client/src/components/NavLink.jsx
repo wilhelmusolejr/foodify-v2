223ea43c98d3e87299f04adc6f7e024b67e818dd
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-11 04:46:20 -0500

    style: make font bigger

--- client/src/components/NavLink.jsx
+++ client/src/components/NavLink.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NavLink({ url = "#", label }) {
   return (
-    <li className="list-none">
+    <li className="list-none text-xl">
       <a href={url}>{label}</a>
     </li>
   );
