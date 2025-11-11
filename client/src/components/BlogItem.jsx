5e34e819376fffabf50936f8b97d8c014217bae4
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-11 01:57:40 -0500

    style: responsive design

--- client/src/components/BlogItem.jsx
+++ client/src/components/BlogItem.jsx
@@ -5,20 +5,18 @@ export default function BlogItem({ heading, date, description, imageUrl }) {
     <div className="my-20 border-b border-black/20 pb-20">
       <div className="flex gap-5 flex-col md:flex-row">
         {/* image */}
-        <div className="h-50 md:min-w-50 md:h-50">
+        <div className="w-full md:w-1/3">
           <img
             src={`images/blog/${imageUrl}`}
             alt={`Image for ${heading}`}
-            className=" object-cover object-center w-full h-full rounded-sm"
+            className="object-cover object-center w-full rounded-sm"
           />
         </div>
 
         {/* data */}
-        <div className="pt-5">
+        <div className="pt-5 flex-1">
           <h3 className="mb-2">{date}</h3>
-          <h2 className="font-bold text-2xl mb-2 poltawski-nowy text-[#2B4A13]">
-            {heading}
-          </h2>
+          <h2 className="font-bold text-2xl mb-2 poltawski-nowy text-[#2B4A13]">{heading}</h2>
           <p className="mb-10  text-[#333]">{description}</p>
           <a href="#" className="underline capitalize text-xl text-[#2B4A13]">
             Continue Reading
