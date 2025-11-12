746933e6d03515a69f697d968d949e8ceb7217c8
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-12 00:57:54 -0500

    feat: add remove functionality

--- client/src/components/IngredientListItem.jsx
+++ client/src/components/IngredientListItem.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faX } from "@fortawesome/free-solid-svg-icons";
 
-export default function ingredientListItem({ name }) {
+export default function ingredientListItem({ name, onRemove }) {
   return (
     <div className="gap-5 py-3 px-4 items-center rounded-lg border border-black/10 w-fit flex">
-      <FontAwesomeIcon icon={faX} />
+      <button onClick={onRemove}>
+        <FontAwesomeIcon icon={faX} />
+      </button>
       <p>{name}</p>
     </div>
   );
