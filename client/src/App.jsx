6975b5e9239ab77a53dfd452fe9c74a34ad6834d
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-12 06:14:27 -0500

    feat: add new page to the dom, category.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
+import Category from "@pages/Category";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/category" element={<Category />} />
       </Routes>
     </BrowserRouter>
   );
