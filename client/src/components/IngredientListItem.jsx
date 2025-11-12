78da0c876a897c59d0ebea16f22fc31003e9911d
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-12 01:34:37 -0500

    style: add cursor pointer to the X button

--- client/src/components/IngredientListItem.jsx
+++ client/src/components/IngredientListItem.jsx
@@ -6,7 +6,7 @@ import { faX } from "@fortawesome/free-solid-svg-icons";
 export default function ingredientListItem({ name, onRemove }) {
   return (
     <div className="gap-5 py-3 px-4 items-center rounded-lg border border-black/10 w-fit flex">
-      <button onClick={onRemove}>
+      <button onClick={onRemove} className="cursor-pointer">
         <FontAwesomeIcon icon={faX} />
       </button>
       <p>{name}</p>
