d42e150f29569650f8193714df253ed26961aa20
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-12 06:13:24 -0500

    feat: add new components.

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import Logo from "@components/Logo";
+
+export default function Footer() {
+  return (
+    <footer className="bg-white w-10/12 mx-auto border border-black/10 rounded-lg mb-30">
+      <div className="flex flex-col lg:flex-row lg:items-center pt-14  gap-14 justify-center min-h-[50vh] w-10/12 max-w-7xl mx-auto">
+        {/* side 1 */}
+        <div className="flex gap-2 flex-col md:w-10/12 max-w-[450px]">
+          <Logo />
+          <p className=" leading-relaxed text-[#333] font-light">
+            “On the other hand, we denouce with righteous indignation and dsilike men who are so
+            beguiled and demoralized by the charms of pleasure of the moment”
+          </p>
+        </div>
+
+        {/* side 2 */}
+        <div className="flex gap-10 flex-col md:flex-row justify-center mb-14 flex-1 md:items-center lg:mb-0">
+          {/* nav 1 */}
+          <div className="uppercase">
+            <h2 className=" text-2xl font-medium mb-5">Browse</h2>
+            <ul className="flex  gap-5 flex-col font-light">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Recipes</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Faq</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+            </ul>
+          </div>
+          {/* nav 2 */}
+          <div className="uppercase">
+            <h2 className=" text-2xl font-medium mb-5">Browse</h2>
+            <ul className="flex  gap-5 flex-col font-light">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Recipes</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Faq</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="border-t text-center border border-black/10">
+        <p className="uppercase py-5">all right reserved.</p>
+      </div>
+    </footer>
+  );
+}
---
+++ client/src/components/MailLetter.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function MailLetter() {
+  return (
+    <div className="pb-20 pt-40 mt-30 bg-black text-white mb-30 relative box-out">
+      <div className="box-in w-60 h-60 absolute left-1/2 -translate-x-1/2 -top-[25%] flex items-center justify-center">
+        <img
+          src="images/burger.png"
+          alt="Burger"
+          className="object-contain object-center w-full h-full"
+        />
+      </div>
+
+      <div className="w-10/12 max-w-96 mx-auto">
+        <h2 className="text-3xl font-medium mb-4">Deliciousness to your inbox</h2>
+        <p>Enjoy weekly hand picked recipes and recommendations.</p>
+
+        {/* form */}
+        <div className="flex gap-3 flex-col mt-4">
+          <input
+            type="text"
+            placeholder="e.g. chicken, rice, broccoli"
+            className="w-full bg-white text-black rounded-lg px-4 py-3"
+          />
+          <button className="bg-white w-full text-black px-4 py-3 rounded-lg">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}
