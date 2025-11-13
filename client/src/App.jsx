383ecfe133eef86093e96b28315afe284f9f6213
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-13 03:38:43 -0500

    refactor: add about route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
 import Category from "@pages/Category";
+import About from "@pages/About";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/category" element={<Category />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
