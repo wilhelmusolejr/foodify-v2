55b921d05578cd95ec32763aff016410c559ab74
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-13 05:29:34 -0500

    refactor: fix spacing and extra space

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Logo() {
   return (
     <>
-    <a href="/" className="logo">
-          <img src="images/logo.png" alt="" className="w-20" />
-        </a>
+      <a href="/" className="logo">
+        <img src="images/logo.png" alt="" className="w-20" />
+      </a>
     </>
   );
 }
