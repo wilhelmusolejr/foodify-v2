d2c882327b86fb2ad6492ebcc436a3f0637b9bef
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-13 05:39:33 -0500

    feat: add component.

---
+++ client/src/components/TeamItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function TeamItem() {
+  return (
+    <div className="">
+      {/* image */}
+      <div className="">
+        <div className="w-40 h-50 bg-black/10 rounded-lg"></div>
+      </div>
+
+      {/* text */}
+      <div className="">
+        <h2 className="capitalize font-medium text-lg mt-2">Member Name</h2>
+        <p className="text-sm font-light">Role</p>
+      </div>
+    </div>
+  );
+}
