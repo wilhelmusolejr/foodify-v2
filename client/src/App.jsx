26b230a467a0895544f5de63c9828c303b391479
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-15 02:12:44 -0500

    feat: add blog route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "@pages/Home";
 import Category from "@pages/Category";
 import About from "@pages/About";
 import Search from "@pages/Search";
+import Blog from "@pages/Blog";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/category" element={<Category />} />
         <Route path="/about" element={<About />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </BrowserRouter>
   );
