d4bff5e4cb810ed400bcf59176e021845c4c06f0
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-17 23:55:57 -0500

    style: font size responsive

--- client/src/components/Recipe/Heading.jsx
+++ client/src/components/Recipe/Heading.jsx
@@ -4,7 +4,7 @@ export default function Heading({ name, step = "" }) {
   return (
     <div className="flex justify-between items-center mb-10 border-b-3 border-green-900 pb-3">
       <div className="">
-        <h2 className="text-xl uppercase font-semibold">{name}</h2>
+        <h2 className="text-2xl lg:text-3xl uppercase font-semibold">{name}</h2>
       </div>
       <div className="">
         <p className="text-lg">{step}</p>
