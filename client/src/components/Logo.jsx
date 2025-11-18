b7a6450ffaa6945f5707bcbed1a2464c27c7adea
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-17 23:46:22 -0500

    refactor: add / to the src url

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <>
       <a href="/" className="logo">
-        <img src="images/logo.png" alt="" className="w-20" />
+        <img src="/images/logo.png" alt="" className="w-20" />
       </a>
     </>
   );
