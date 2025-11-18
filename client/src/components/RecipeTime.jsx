bcb25c8ef483509774a17f28f46643e2739a6ad1
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-18 09:18:48 -0500

    feat: add conditionals if time is missing

--- client/src/components/RecipeTime.jsx
+++ client/src/components/RecipeTime.jsx
@@ -4,6 +4,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faClock } from "@fortawesome/free-solid-svg-icons";
 
 export default function RecipeTime({ heading, time }) {
+  if (time === null) {
+    return;
+  }
+
   return (
     <div className="flex items-center gap-3">
       <FontAwesomeIcon icon={faClock} className="text-green-900" size="2xl" />
