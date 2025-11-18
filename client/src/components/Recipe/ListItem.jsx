2e88e4a5a43faa48ee5c391323c1a0c93e08ad04
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-18 02:10:47 -0500

    style: remove capitalization

--- client/src/components/Recipe/ListItem.jsx
+++ client/src/components/Recipe/ListItem.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export default function ListItem({ children }) {
-  return (
-    <li className="capitalize flex items-center gap-3 text-lg lg:text-xl font-light">{children}</li>
-  );
+  return <li className="flex items-center gap-3 text-lg lg:text-xl font-light">{children}</li>;
 }
