b908221643833a9fad5a63e476449fe2fd86031f
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-18 02:10:25 -0500

    refactor: modify to the actual content

--- client/src/components/RecipeTime.jsx
+++ client/src/components/RecipeTime.jsx
@@ -3,13 +3,13 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faClock } from "@fortawesome/free-solid-svg-icons";
 
-export default function RecipeTime() {
+export default function RecipeTime({ heading, time }) {
   return (
     <div className="flex items-center gap-3">
       <FontAwesomeIcon icon={faClock} className="text-green-900" size="2xl" />
       <div className="font-medium">
-        <p className="uppercase text-lg text-[#444]">Prep time</p>
-        <h3 className="text-xl ">15 minutes</h3>
+        <p className="uppercase text-lg text-[#444]">{heading}</p>
+        <h3 className="text-xl ">{time} minutes</h3>
       </div>
     </div>
   );
