d36dada303481d4d101fe7ea51a8664fdfd584d4
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-18 05:29:37 -0500

    feat: add dynamic data

--- client/src/components/HeadingImage.jsx
+++ client/src/components/HeadingImage.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-export default function HeadingImage({ image_path = "" }) {
+export default function HeadingImage({ image_name = "", id }) {
+  let image_path;
+
+  if (image_name.includes("https")) {
+    image_path = image_name;
+  } else {
+    image_path = `https://img.spoonacular.com/recipes/${image_name}`;
+  }
+
+  let recipe_link = `http://localhost:5173/recipe/${id}`;
+
   return (
-    <div className={`min-w-[200px] h-[150px] rounded-md`}>
+    <a href={recipe_link} className={`min-w-[200px] h-[150px] rounded-md`}>
       <img
         src={image_path}
         alt={`Image for header food item`}
         className=" object-cover object-center w-full h-full rounded-sm"
       />
-    </div>
+    </a>
   );
 }
