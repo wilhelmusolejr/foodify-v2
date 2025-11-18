7105f4d1c369b3006c7a2a6ffd6ab13299cbe317
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-17 23:53:09 -0500

    style: font size responsive

--- client/src/components/NutritionFacts.jsx
+++ client/src/components/NutritionFacts.jsx
@@ -4,11 +4,11 @@ export default function NutritionFacts({ className }) {
   return (
     <div className={`px-7 py-10 bg-white rounded-lg border border-black/10 shadow-md ${className}`}>
       <div className="">
-        <h2 className="text-xl uppercase font-semibold">Nutrition facts</h2>
+        <h2 className="text-2xl lg:text-3xl uppercase font-semibold">Nutrition facts</h2>
       </div>
 
-      <ul className="mt-5 flex gap-2 flex-col">
-        <li className="flex mb-2 border-b border-black/10 justify-between">
+      <ul className="mt-5 flex gap-5 flex-col">
+        <li className="flex border-b border-black/10 justify-between text-lg lg:text-xl">
           <div className="">
             <p>Calories</p>
           </div>
@@ -16,7 +16,7 @@ export default function NutritionFacts({ className }) {
             <p>200 kcal</p>
           </div>
         </li>
-        <li className="flex mb-2 border-b border-black/10 justify-between">
+        <li className="flex border-b border-black/10 justify-between text-lg lg:text-xl">
           <div className="">
             <p>Calories</p>
           </div>
@@ -24,7 +24,7 @@ export default function NutritionFacts({ className }) {
             <p>200 kcal</p>
           </div>
         </li>
-        <li className="flex mb-2 border-b border-black/10 justify-between">
+        <li className="flex border-b border-black/10 justify-between text-lg lg:text-xl">
           <div className="">
             <p>Calories</p>
           </div>
