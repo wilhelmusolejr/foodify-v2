30b266ca56c1587b9432aed14829b5abc491e6c3
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-17 23:46:42 -0500

    style: font size responsive

--- client/src/components/Recipe/ListItem.jsx
+++ client/src/components/Recipe/ListItem.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function ListItem({ children }) {
-  return <li className="capitalize flex items-center gap-3">{children}</li>;
+  return (
+    <li className="capitalize flex items-center gap-3 text-lg lg:text-xl font-light">{children}</li>
+  );
 }
