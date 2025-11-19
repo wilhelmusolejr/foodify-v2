f1acb5756acbddd998e0a584f5297082e4e1de6a
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-18 23:10:55 -0500

    style: show all tags with color red and green to indicate true or false

--- client/src/components/IconItem.jsx
+++ client/src/components/IconItem.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function IconItem({ icon, name }) {
+export default function IconItem({ icon, name, status }) {
+  let color = status === false ? "red" : "green";
+
   return (
-    <div className="bg-green-100 text-green-800 p-5 rounded-lg text-center shadow-sm flex items-center justify-center flex-col">
-      <FontAwesomeIcon icon={icon} className="mb-2" size="2xl" />
+    <div
+      className={`bg-${color}-100 aspect-square text-${color}-800 p-2 rounded-lg text-center shadow-sm flex items-center justify-center flex-col`}
+    >
+      <FontAwesomeIcon icon={icon} className="mb-2" size="xl" />
       <h3 className="font-semibold text-base capitalize">{name}</h3>
     </div>
   );
