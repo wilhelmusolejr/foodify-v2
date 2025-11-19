d52aff6f77742436407ff97779eaa6adfb81541d
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-19 06:29:16 -0500

    bug: fix color not showing

--- client/src/components/IconItem.jsx
+++ client/src/components/IconItem.jsx
@@ -3,11 +3,19 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function IconItem({ icon, name, status }) {
-  let color = status === false ? "red" : "green";
+  const colorClasses = status
+    ? {
+        bg: "bg-green-100",
+        text: "text-green-800",
+      }
+    : {
+        bg: "bg-red-100",
+        text: "text-red-800",
+      };
 
   return (
     <div
-      className={`bg-${color}-100 aspect-square text-${color}-800 p-2 rounded-lg text-center shadow-sm flex items-center justify-center flex-col`}
+      className={`aspect-square p-2 rounded-lg text-center shadow-sm flex items-center justify-center flex-col ${colorClasses.bg} ${colorClasses.text}`}
     >
       <FontAwesomeIcon icon={icon} className="mb-2" size="xl" />
       <h3 className="font-semibold text-base capitalize">{name}</h3>
