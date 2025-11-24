75a52c308c51b4514133e47f3d12ed17a73fa0d3
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-24 05:43:49 -0500

    style: add icons in skeleton

--- client/src/components/RecipeItemSkeleton.jsx
+++ client/src/components/RecipeItemSkeleton.jsx
@@ -1,14 +1,49 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBacon,
+  faBowlRice,
+  faBreadSlice,
+  faBurger,
+  faCarrot,
+  faCheese,
+  faCookie,
+  faEgg,
+  faFish,
+  faIceCream,
+  faMartiniGlass,
+  faUtensils,
+} from "@fortawesome/free-solid-svg-icons";
+
 export default function RecipeItemSkeleton() {
+  let icons = [
+    faBacon,
+    faBowlRice,
+    faBurger,
+    faCarrot,
+    faCheese,
+    faBreadSlice,
+    faUtensils,
+    faFish,
+    faCookie,
+    faIceCream,
+    faMartiniGlass,
+    faEgg,
+  ];
+
+  let icon = icons[Math.floor(Math.random() * icons.length)];
+
   return (
     <div className="animate-pulse">
       {/* Mirrors: <div className="aspect-square ..."> and <img> */}
-      <div className="aspect-square rounded-lg flex items-center justify-center">
+      <div className="aspect-square rounded-lg flex items-center relative justify-center">
         <div
           // Match the size and shape of the image container
-          className="w-full h-full rounded-sm bg-gray-300 border border-black/10"
-        />
+          className="w-full h-full rounded-sm bg-gray-300 border border-black/10 absolute inset-0 flex justify-center items-center"
+        >
+          <FontAwesomeIcon icon={icon} size="2x" />
+        </div>
       </div>
 
       {/* 2. Title Placeholder */}
