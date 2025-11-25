29ec04dc25cf4474f766162f1a6c3f6dca7fba63
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-25 00:12:03 -0500

    style: design for disabled button

--- client/src/components/SearchButton.jsx
+++ client/src/components/SearchButton.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-export default function SearchButton({ onClick, className = "" }) {
+export default function SearchButton({ onClick, className = "", isDisabled = false }) {
   return (
     <button
-      className={`bg-black w-full md:w-fit cursor-pointer text-white px-4 py-3 rounded-lg uppercase ${className}`}
+      className={`bg-black w-full md:w-fit  ${
+        isDisabled
+          ? "bg-gray-400 cursor-not-allowed opacity-70"
+          : "bg-black hover:bg-gray-800 cursor-pointer"
+      } text-white px-4 py-3 rounded-lg uppercase ${className}`}
       onClick={onClick}
+      disabled={isDisabled}
     >
       <p>Search</p>
     </button>
