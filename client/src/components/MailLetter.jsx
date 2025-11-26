72d07cf21c50740e9427b8d1a47dc24300e89426
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-26 08:39:52 -0500

    style: consume button component, design add

--- client/src/components/MailLetter.jsx
+++ client/src/components/MailLetter.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
+import Button from "./Global/Button";
+
 export default function MailLetter() {
   return (
-    <div className="pb-20 pt-40 my-40 bg-black text-white relative box-out">
+    <div className="pb-20 pt-40 my-40 bg-green-950 border-y-8 border-green-700 text-white relative box-out">
       <div className="box-in w-60 h-60 absolute left-1/2 -translate-x-1/2 -top-[25%] flex items-center justify-center">
         <img
           src="/images/burger.png"
@@ -13,16 +15,16 @@ export default function MailLetter() {
 
       <div className="w-10/12 max-w-96 mx-auto">
         <h2 className="text-3xl font-medium mb-4">Deliciousness to your inbox</h2>
-        <p>Enjoy weekly hand picked recipes and recommendations.</p>
+        <p className="text-[#f4f4f4]">Enjoy weekly hand picked recipes and recommendations.</p>
 
         {/* form */}
         <div className="flex gap-3 flex-col mt-4">
           <input
             type="text"
-            placeholder="e.g. chicken, rice, broccoli"
+            placeholder="foodify@gmail.com"
             className="w-full bg-white text-black rounded-lg px-4 py-3"
           />
-          <button className="bg-white w-full text-black px-4 py-3 rounded-lg">Get Started</button>
+          <Button>Get Started</Button>
         </div>
       </div>
     </div>
