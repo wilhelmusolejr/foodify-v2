51daaa6283e2f29c8ff3bfb67b2df08abde80bad
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-26 09:35:19 -0500

    style: add hover zoom in and light rotate

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
 export default function CategoryItem({ image_path, title }) {
-  let link = `http://localhost:5173/search?query=${title}`;
+  let FRONTEND_HOST_URL = import.meta.env.VITE_FRONTEND_URL;
+
+  let link = `${FRONTEND_HOST_URL}/search?query=${title}`;
 
   return (
-    <a href={link} className="text-center flex flex-col items-center gap-4">
-      {/* image */}
-      <div className="w-30 h-30 lg:w-40 lg:h-40 xl:w-[200px] xl:h-[200px] rounded-full">
+    <a href={link} className="text-center flex flex-col items-center gap-4 group">
+      {/* image wrapper */}
+      <div className="w-30 h-30 lg:w-40 lg:h-40 xl:w-[200px] xl:h-[200px] rounded-full overflow-hidden">
         <img
           src={image_path}
-          alt={`Image for header food item`}
-          className=" object-cover object-center w-full h-full rounded-sm"
+          alt={`Image for ${title}`}
+          className="
+            w-full h-full object-cover object-center rounded-full
+            transition-all duration-500 ease-out
+            group-hover:scale-110 
+            group-hover:rotate-3
+          "
         />
       </div>
 
