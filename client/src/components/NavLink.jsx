038a29a8bf4b95ccb61419bba5ae44872213deb5
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-26 07:14:58 -0500

    style: add font bold when page is in the navigator. and some hover effects

--- client/src/components/NavLink.jsx
+++ client/src/components/NavLink.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function NavLink({ url = "#", label }) {
+  const location = useLocation();
+  console.log(location.pathname);
+
+  let className =
+    location.pathname === url
+      ? "text-green-500 font-semibold"
+      : "text-gray-500 hover:text-green-400";
+
   return (
-    <li className="list-none text-xl">
+    <li className={`list-none text-xl ${className}`}>
       <a href={url}>{label}</a>
     </li>
   );
