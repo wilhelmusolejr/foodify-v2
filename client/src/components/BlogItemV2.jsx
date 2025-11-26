8697995264c3aa40096ba40718af8d51b625d752
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-26 09:44:15 -0500

    pending.

--- client/src/components/BlogItemV2.jsx
+++ client/src/components/BlogItemV2.jsx
@@ -2,29 +2,26 @@ import React from "react";
 
 import Paragraph from "@components/Paragraph";
 
-export default function BlogItemV2() {
+export default function BlogItemV2({ data }) {
+  const { header, date, image_name, description } = data;
+
   return (
     <div className="bg-white p-5 py-10 rounded-lg shadow-md">
       {/* image */}
-      <div className="w-full ">
+      <div className="w-full max-h-[200px] overflow-hidden">
         <img
-          src={`images/blog/blog1.jpg`}
+          src={`images/blog/${image_name}`}
           alt={`Image for `}
-          className="object-cover object-center w-full rounded-sm"
+          className="object-cover object-center w-full h-full rounded-sm"
         />
       </div>
 
       {/* data */}
       <div className="pt-5 flex-1">
-        <h3 className="mb-4 uppercase font-medium">November 30, 2025</h3>
-        <h2 className="font-bold text-3xl mb-2 poltawski-nowy text-[#2B4A13]">
-          29 cozy soups and strews
-        </h2>
+        <h3 className="mb-4 uppercase font-medium">{date}</h3>
+        <h2 className="font-bold text-3xl mb-2 poltawski-nowy text-[#2B4A13]">{header}</h2>
 
-        <Paragraph className="mx-auto mb-10">
-          Warm your bones with 29 hearty soups and stews designed for chilly evenings. From creamy
-          tomato bisque to slow-simmered beef stew, these easy-to-make recipes use pantry.
-        </Paragraph>
+        <Paragraph className="mx-auto mb-10">{description}</Paragraph>
 
         <a href="#" className="underline capitalize text-xl text-[#2B4A13]">
           Continue Reading
