cdf4ce5ebc5a6f1be60a4c8bc1a73528dfdf7c06
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-26 08:39:28 -0500

    style: ajust footer + change link to soc links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,6 +3,25 @@ import React from "react";
 import Logo from "@components/Logo";
 
 export default function Footer() {
+  const socialLinks = [
+    {
+      name: "portfolio",
+      link: "https://wilhelmus.vercel.app",
+    },
+    {
+      name: "facebook",
+      link: "https://facebook.com/wilhelmus.ole",
+    },
+    {
+      name: "GitHub",
+      link: "https://github.com/wilhelmusolejr",
+    },
+    {
+      name: "LinkedIn",
+      link: "https://linkedin.com/wilhelmusolejr",
+    },
+  ];
+
   return (
     <footer className="bg-white w-10/12 mx-auto border border-black/10 rounded-lg mb-30">
       <div className="flex flex-col lg:flex-row lg:items-center pt-14  gap-14 justify-center min-h-[50vh] w-10/12 max-w-7xl mx-auto">
@@ -16,7 +35,7 @@ export default function Footer() {
         </div>
 
         {/* side 2 */}
-        <div className="flex gap-10 flex-col md:flex-row justify-center mb-14 flex-1 md:items-center lg:mb-0">
+        <div className="flex gap-10 flex-col md:flex-row justify-center mb-14 flex-1 md:items-start lg:mb-0">
           {/* nav 1 */}
           <div className="uppercase">
             <h2 className=" text-2xl font-medium mb-5">Browse</h2>
@@ -40,29 +59,21 @@ export default function Footer() {
           </div>
           {/* nav 2 */}
           <div className="uppercase">
-            <h2 className=" text-2xl font-medium mb-5">Browse</h2>
-            <ul className="flex  gap-5 flex-col font-light">
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">Recipes</a>
-              </li>
-              <li>
-                <a href="#">Blog</a>
-              </li>
-              <li>
-                <a href="#">Faq</a>
-              </li>
-              <li>
-                <a href="#">About</a>
-              </li>
+            <h2 className=" text-2xl font-medium mb-5">Connect With Us</h2>
+            <ul className="flex gap-5 flex-col font-light">
+              {socialLinks.map((social, index) => (
+                <li key={index + social.name}>
+                  <a href={social.link} target="_blank" rel="noopener noreferrer">
+                    {social.name}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
       </div>
 
-      <div className="border-t text-center border border-black/10">
+      <div className="border-t text-center text-white border border-black/10 bg-green-900">
         <p className="uppercase py-5">all right reserved.</p>
       </div>
     </footer>
