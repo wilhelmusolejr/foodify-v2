28133d6c1097c53a97ca6b81a62650f54a5d67a6
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-26 03:05:39 -0500

    feat: add provider for react usequery

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { ModalProvider } from "./context/ModalContext.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import App from "./App.jsx";
+import "./index.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ModalProvider>
-      <App />
-    </ModalProvider>
+    <QueryClientProvider client={queryClient}>
+      <ModalProvider>
+        <App />
+      </ModalProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
