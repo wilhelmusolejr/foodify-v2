1fe429e31acaaae5e109acbba60e0652bc3d9ff2
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-26 09:29:20 -0500

    style: add image scale when hover

--- client/src/components/RecipeItem.jsx
+++ client/src/components/RecipeItem.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { faEgg } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { randomFoodIcon } from "../utils/foodLoaderUtils";
@@ -22,23 +21,24 @@ export default function RecipeItem({ image_name, id, name }) {
   let recipe_link = `http://localhost:5173/recipe/${id}`;
 
   return (
-    <>
-      <a href={recipe_link} className="">
-        <div className="aspect-square rounded-lg flex items-center justify-center border border-black/10 relative">
-          {image_path ? (
-            <img
-              src={image_path}
-              alt={`Image for header food item`}
-              className=" object-cover object-center w-full h-full rounded-sm border border-black/10 bg-white"
-            />
-          ) : (
-            <div className="absolute inset-0 bg-white flex items-center justify-center">
-              <FontAwesomeIcon icon={icon} size="2x" className="text-black/50" />
-            </div>
-          )}
-        </div>
-        <h2 className="capitalize mt-2 text-lg font-medium">{name}</h2>
-      </a>
-    </>
+    <a href={recipe_link} className="group block">
+      <div className="aspect-square rounded-lg flex items-center justify-center border border-black/10 relative overflow-hidden">
+        {image_path ? (
+          <img
+            src={image_path}
+            alt={`Image for header food item`}
+            className="object-cover object-center w-full h-full rounded-sm border border-black/10 bg-white 
+                     transition-transform duration-300 ease-out 
+                     group-hover:scale-105"
+          />
+        ) : (
+          <div className="absolute inset-0 bg-white flex items-center justify-center">
+            <FontAwesomeIcon icon={icon} size="2x" className="text-black/50" />
+          </div>
+        )}
+      </div>
+
+      <h2 className="capitalize mt-2 text-lg font-medium">{name}</h2>
+    </a>
   );
 }
