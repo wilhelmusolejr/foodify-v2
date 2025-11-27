4e336a6d017230b6938f4522201735b904d5c226
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-26 20:45:18 -0500

    style: adjust height of the img

--- client/src/components/BlogItemV2.jsx
+++ client/src/components/BlogItemV2.jsx
@@ -6,24 +6,25 @@ export default function BlogItemV2({ data }) {
   const { header, date, image_name, description } = data;
 
   return (
-    <div className="bg-white p-5 py-10 rounded-lg shadow-md">
+    <div className="bg-white  rounded-lg shadow-md flex flex-col">
       {/* image */}
-      <div className="w-full max-h-[200px] overflow-hidden">
+      <a href="#" className="w-full h-[230px] overflow-hidden group">
         <img
           src={`images/blog/${image_name}`}
-          alt={`Image for `}
-          className="object-cover object-center w-full h-full rounded-sm"
+          alt={header}
+          className="object-cover object-center w-full h-full rounded-t-lg transition-all duration-500 ease-out group-hover:scale-110 "
         />
-      </div>
+      </a>
 
       {/* data */}
-      <div className="pt-5 flex-1">
+      <div className="flex flex-col flex-1 p-5 py-10">
         <h3 className="mb-4 uppercase font-medium">{date}</h3>
         <h2 className="font-bold text-3xl mb-2 poltawski-nowy text-[#2B4A13]">{header}</h2>
 
         <Paragraph className="mx-auto mb-10">{description}</Paragraph>
 
-        <a href="#" className="underline capitalize text-xl text-[#2B4A13]">
+        {/* Push link to bottom */}
+        <a href="#" className="underline capitalize text-xl text-[#2B4A13] mt-auto">
           Continue Reading
         </a>
       </div>
