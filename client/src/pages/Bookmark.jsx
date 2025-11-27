e6097f531eed6548aa8241f76102f80e57304d72
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-27 09:43:16 -0500

    pending

--- client/src/pages/Bookmark.jsx
+++ client/src/pages/Bookmark.jsx
@@ -9,6 +9,9 @@ import SectionHeading from "@components/SectionHeading";
 import RecipeItemSkeleton from "@components/RecipeItemSkeleton";
 import MailLetter from "@components/MailLetter";
 import RecipeItem from "@components/RecipeItem";
+import Label from "@components/Label";
+import SearchButton from "@components/SearchButton";
+import Footer from "@components/Footer";
 
 import axios from "axios";
 import { useParams } from "react-router-dom";
@@ -91,14 +94,47 @@ export default function Bookmark() {
         {/* heading */}
         <SectionHeading heading="Bookmark" subheading="Browse All Recipes by Category or Filter" />
 
-        <div className="flex flex-col gap-3 mb-14 md:flex-row max-w-7xl mx-auto">
-          <div className="p-5 border border-black/30 rounded-lg flex items-center justify-between gap-2">
-            <p>Recipes</p>
-            <FontAwesomeIcon icon={faChevronDown} size="1x" />
+        {/* option */}
+        <div className="mb-5 lg:mb-10 mt-30  max-w-7xl mx-auto flex flex-col-reverse lg:flex-row lg:items-end gap-10">
+          {/* option 1 */}
+          <div className="flex flex-col gap-3 md:flex-row w-full md:justify-between lg:justify-start">
+            {/* drop down */}
+            <div className="relative">
+              <select className="appearance-none px-4 py-3 w-fullborder border-black/30 rounded-md pr-1 cursor-pointer">
+                <option value="recipe" selected>
+                  Recipe
+                </option>
+                <option value="blog">Blog</option>
+              </select>
+
+              {/* Custom arrow */}
+              <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center text-black/60">
+                <FontAwesomeIcon icon={faChevronDown} size="1x" />
+              </div>
+            </div>
+
+            {/* generate */}
+            <button className="px-4 py-3 border border-black/30 rounded-lg w-full md:w-fit">
+              Generate Shopping List
+            </button>
           </div>
-          <button className="p-5 border border-black/30 rounded-lg w-full md:w-fit">
-            Generate Shopping List
-          </button>
+
+          {/* Search  */}
+          <div className="flex flex-col md:flex-row md:items-end justify-center gap-2">
+            {/* form */}
+            <div className="w-full md:w-fit flex flex-col gap-2 ">
+              <Label name="Recipe name" required={true} />
+              <input
+                type="text_name"
+                placeholder="e.g. chicken, rice, broccoli"
+                className={`border flex-1 md:w-fit border-black/50 rounded-lg px-4 py-3 lg:min-w-80`}
+                // value={searchInput}
+                // onChange={(e) => setSearchInput(e.target.value)}
+              />
+            </div>
+            {/* button */}
+            <SearchButton />
+          </div>
         </div>
 
         {/* recipe */}
@@ -127,9 +163,56 @@ export default function Bookmark() {
             </>
           )}
         </div>
+
+        {/* Pagination */}
+        {searchResults.totalResults > 10 && (
+          <div className="flex justify-center items-center gap-3 my-30">
+            {pageNum > 1 && (
+              <PaginationButton
+                pageNum={pageNum - 1}
+                onClick={handlePagination}
+                className="px-3"
+                isDisabled={pageNum <= 1}
+                text={"Previous"}
+              />
+            )}
+            {pageNum > 1 && (
+              <PaginationButton
+                pageNum={pageNum - 1}
+                text={pageNum - 1}
+                onClick={handlePagination}
+              />
+            )}
+
+            <PaginationButton
+              pageNum={pageNum}
+              text={pageNum}
+              isDisabled={true}
+              className={"bg-black text-white"}
+            />
+
+            {pageNum < searchResults.pageLimit && (
+              <>
+                <PaginationButton
+                  pageNum={pageNum + 1}
+                  text={pageNum + 1}
+                  onClick={handlePagination}
+                />
+                <PaginationButton
+                  pageNum={pageNum + 1}
+                  text={"Next"}
+                  onClick={handlePagination}
+                  className="px-3"
+                />
+              </>
+            )}
+          </div>
+        )}
       </div>
 
       <MailLetter />
+
+      <Footer />
     </>
   );
 }
