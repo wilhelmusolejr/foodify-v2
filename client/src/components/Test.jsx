a74369daa5723e5e04ae2861461d51cda99e4b80
Wilhelmus Ole (acer) <wilhelmus.olejr@gmail.com>
2025-11-26 20:45:30 -0500

    style: add hover effects

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -6,13 +6,16 @@ export default function Test({ name, image_name }) {
   let link = `http://localhost:5173/search?query=${name}`;
 
   return (
-    <div className="text-center flex flex-col items-center gap-5 w-full">
+    <div className="text-center flex flex-col items-center gap-5 w-full group">
       {/* image */}
-      <a href={link} className="rounded-full">
+      <a href={link} className="rounded-full overflow-hidden">
         <img
           src={image_path}
           alt={`Image for header food item`}
-          className=" object-cover border border-black/10 shadow-md object-center w-full h-full rounded-full aspect-square bg-white"
+          className=" object-cover border border-black/10 shadow-md aspect-square bg-white w-full h-full object-center rounded-full
+            transition-all duration-500 ease-out
+            group-hover:scale-110 
+            group-hover:rotate-3"
         />
       </a>
 
