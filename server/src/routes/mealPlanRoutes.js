67c8748ed01a6631c4c11470d1abed3ec51edece
wilhelmusolejr <wilhelmus.olejr@gmail.com>
2025-12-18 18:25:26 +0800

    feat: add deleteusermeal route

--- server/src/routes/mealPlanRoutes.js
+++ server/src/routes/mealPlanRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { getUserMeal, addUserMeal } from "../controllers/mealPlanController.js";
+import {
+  getUserMeal,
+  addUserMeal,
+  deleteUserMeal,
+} from "../controllers/mealPlanController.js";
 import authMiddleware from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/usermeal", authMiddleware, getUserMeal);
 router.post("/usermeal", authMiddleware, addUserMeal);
+router.delete("/usermeal", authMiddleware, deleteUserMeal);
 
 export default router;
