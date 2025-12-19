c0792fadb792070ff06e25eb6c8148528e8d9d41
wilhelmusolejr <wilhelmus.olejr@gmail.com>
2025-12-20 07:49:36 +0800

    style: remove emoji on card

--- client/src/components/mealplanner/MealTypeItem.jsx
+++ client/src/components/mealplanner/MealTypeItem.jsx
@@ -7,7 +7,7 @@ export default function MealTypeItem({ mealTypeName = "loading", mealNum = 0 }) 
   return (
     <li className="flex items-center justify-between">
       <div className="flex gap-1 items-center">
-        <FontAwesomeIcon icon={icon} size="xs" />
+        {/* <FontAwesomeIcon icon={icon} size="xs" /> */}
         <span className="text-gray-700">{mealTypeName}</span>
       </div>
       <span className="px-2 py-0.5 text-xs bg-blue-100 text-blue-600 rounded-full">{mealNum}</span>
